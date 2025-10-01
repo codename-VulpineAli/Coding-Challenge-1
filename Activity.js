74e080a77d1ed3cbe2de97a3e4a7069ddfd37c29
Ali <allynacabello>
2025-10-01 14:25:24 +0100

    Coding challenge 1

--- Activity.js
+++ Activity.js
@@ -1,10 +1,23 @@
 //Activity 1
 
+// Scenario
+// Mark and John are trying to compare their Body Mass Indexes (BMI).
+
+// BMI is calculated with the formula:
+
+
+// Where:
+
+// mass is in kilograms (kg)
+
+// height is in meters (m)
+
 // Test Data 1
-const markMass1 = 78;
-const markHeight1 = 1.69;
-const johnMass1 = 92;
-const johnHeight1 = 1.95;
+// Store Mark's and John's mass and height in variables.
+const markMass1 = 70;
+const markHeight1 = 1.66;
+const johnMass1 = 39;
+const johnHeight1 = 1.40;
 
 const markBMI1 = markMass1 / (markHeight1 ** 2);
 const johnBMI1 = johnMass1 / (johnHeight1 ** 2);
@@ -19,17 +32,18 @@ console.log("Is Mark's BMI higher?", markHigherBMI1);
 // -------------------------
 
 // Test Data 2
-const markMass2 = 95;
-const markHeight2 = 1.88;
-const johnMass2 = 85;
-const johnHeight2 = 1.76;
+const markMass2 = 70;
+const markHeight2 = 1.66;
+const johnMass2 = 39;
+const johnHeight2 = 1.40;
 
 const markBMI2 = markMass2 / (markHeight2 ** 2);
 const johnBMI2 = johnMass2 / (johnHeight2 ** 2);
 
-const markHigherBMI2 = markBMI2 > johnBMI2;
+const markHigherBMI2 = markBMI2 < johnBMI2;
 
-console.log("\nTest Data 2:");
+console.log("Test Data 2:");
 console.log("Mark's BMI:", markBMI2);
 console.log("John's BMI:", johnBMI2);
 console.log("Is Mark's BMI higher?", markHigherBMI2);
+
